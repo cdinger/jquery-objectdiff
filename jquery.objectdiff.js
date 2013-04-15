e92506bf8859a3c9c8b1ac5706074f0e1eb78af2
Robin Franken <franken@lequest.nl>
2013-04-15 10:19:50 +0200

    Added default objects if undefined. This should fix issue #2

--- jquery.objectdiff.js
+++ jquery.objectdiff.js
@@ -1,16 +1,16 @@
 /**
  * jquery-objectdiff  - a jQuery plugin
  *
- * version 1.0.1
+ * version 1.0.2
  *
  * This plugin is just a helper function for calculating a diff of two similar
  * objects ala ActiveRecord's changes hash. Just pass in two objects and this
- * function will return an object of changes that contains only the properties 
- * that have changed. Differences are stored in arrays; the first element has 
+ * function will return an object of changes that contains only the properties
+ * that have changed. Differences are stored in arrays; the first element has
  * the 'before' value and the second element is the 'after' value.
- * 
+ *
  * Usage:
- * 
+ *
  *    var before = {id:123, name:{first:"Johnny", last:"Johnson"}};
  *    var after = {id:123, name:{first:"John", last:"Johnson"}, age:30};
  *    var changes = $.objectDiff(before, after);
@@ -20,23 +20,25 @@
  *    {name: {first:["Johnny","John"]}, age:[null,30]}
  *
  */
-(function($){ 
-  $.objectDiff = function(a, b, c) {
-    c = {};
-    $.each([a, b], function(index, obj) {
-      for (prop in obj) {
-        if (obj.hasOwnProperty(prop)) {
-          if (typeof obj[prop] === "object" && obj[prop] !== null) {
-            c[prop] = $.objectDiff(a[prop], b[prop], c);
-          }
-          else {
-            if (a[prop] !== b[prop]) {
-              c[prop] = [a[prop], b[prop]];
+(function ($) {
+    $.objectDiff = function (a, b, c) {
+        c = {};
+        $.each([a, b], function (index, obj) {
+            for (prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    if (typeof obj[prop] === "object" && obj[prop] !== null) {
+                        c[prop] = $.objectDiff(a[prop], b[prop], c);
+                    }
+                    else {
+                        if(a===undefined) a = {};
+                        if(b===undefined) b = {};
+                        if (a[prop] !== b[prop]) {
+                            c[prop] = [a[prop], b[prop]];
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    });
-    return c;
-  }
+        });
+        return c;
+    }
 })(jQuery);
